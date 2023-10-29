dbd0568a7da805782741cfd7d0c941d5ca235738
Kotokogotka <asics038@yandex.ru>
2023-10-29 21:58:44 +0300

    Добавлен хандлер админ и юзер, добавлены папки с хандлерами

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from loader import dp
+from aiogram.types import ReplyKeyboardRemove
 
 from keyboard.keyboard import start_keyboard
 from lexicon.lexiconRu import text_command
@@ -12,6 +13,16 @@ async def process_start_command(message: types.Message):
 
 
 @dp.message_handlers(text=text_command['admin_message'])
-async def admin_mode_process(messag: types.Message):
-    admin_id = messag.chat.id
+async def admin_mode_process(message: types.Message):
+    admin_id = message.chat.id
     if admin_id not in ADMINS:
+        ADMINS.append(admin_id)
+    await message.answer(text_command['on_admin'], reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handlers(text=text_command['user_message'])
+async def admin_mode_process(message: types.Message):
+    admin_id = message.chat.id
+    if admin_id not in ADMINS:
+        ADMINS.append(admin_id)
+    await message.answer(text_command['on_user'], reply_markup=ReplyKeyboardRemove())
--- keyboard/keyboard.py
+++ keyboard/keyboard.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup
-from lexicon.lexiconRu import text_command
+from Magazin_bot.lexicon.lexiconRu import text_command
 
 start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 start_keyboard.row(text_command['user_message'], text_command['admin_message'])
